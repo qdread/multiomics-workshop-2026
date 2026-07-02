524f536b72bc05d80776a2f566e3a7f30b5c8d7b
Quentin Read <quentin.read@usda.gov>
2026-07-02 14:38:49 -0400

    Intro slides and notebook for part 2

--- setup.R
+++ setup.R
@@ -1,4 +1,4 @@
 # Script to install all needed packages for the lessons
-install.packages(c('BiocManager', 'ggplot2', 'RColorBrewer', 'gplots'))
+install.packages(c('BiocManager', 'ggplot2', 'RColorBrewer', 'gplots', 'ggrepel'))
 
 BiocManager::install(c('mixOmics', 'limma', 'edgeR', 'Mus.musculus', 'ComplexHeatmap'))
